9fd7cc3a209b71edad035abc6aa8b60baea19254
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-01-25 05:07:38 +0200

    Added LED Driver files.

---
+++ ATMega32_MC and Interfacing Drivers/ATMega32_MC and Interfacing Drivers/HAL/LED/LED Driver.c
@@ -0,0 +1,21 @@
+#include "../../Utilities/AVR_ATMega32.h"
+
+void LED_Init(U8 port, U8 pin){
+	DIO_PinDirection(port, pin, OUTPUT);
+}
+
+void LEDs_Init(U8 port, U8 stPin, U8 endPin){
+	DIO_PortRangeDirection(port, stPin, endPin, OUTPUT);
+}
+
+void Set_LED(U8 port, U8 pin, U8 value){
+	DIO_PinWrite(port, pin, value);
+}
+
+void Set_LEDs(U8 port, U8 stPin, U8 endPin, U8 value){
+	DIO_PortRangeWrite(port, stPin, endPin, value);
+}
+
+
+
+
---
+++ ATMega32_MC and Interfacing Drivers/ATMega32_MC and Interfacing Drivers/HAL/LED/LED Driver.h
@@ -0,0 +1,13 @@
+#ifndef LED_DRIVER_H
+#define LED_DRIVER_H
+
+#include "../../Utilities/AVR_ATMega32.h"
+
+void LED_Init(U8, U8);
+void LED_Init(U8);
+
+void Set_LED(U8, U8, U8);
+void Set_LED(U8 ,U8);
+
+
+#endif
